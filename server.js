9b51c05304bf809dce750d3014c5536027795ba3
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-11-09 11:36:39 -0500

    server.js

--- server.js
+++ server.js
@@ -15,32 +15,32 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
   })
 
-// Set up sessions with cookies
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {
-      // Stored in milliseconds
-      maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize,
-    }),
-  };
+// // Set up sessions with cookies
+// const sess = {
+//     secret: 'Super secret secret',
+//     cookie: {
+//       // Stored in milliseconds
+//       maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+//     },
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//       db: sequelize,
+//     }),
+//   };
   
-  app.use(session(sess));
+//   app.use(session(sess));
   
-  const hbs = exphbs.create({ helpers });
+//   const hbs = exphbs.create({ helpers });
   
-  app.engine('handlebars', hbs.engine);
-  app.set('view engine', 'handlebars');
+//   app.engine('handlebars', hbs.engine);
+//   app.set('view engine', 'handlebars');
   
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, 'public')));
+//   app.use(express.json());
+//   app.use(express.urlencoded({ extended: true }));
+//   app.use(express.static(path.join(__dirname, 'public')));
   
-  app.use(routes);
+//   app.use(routes);
   
   sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () =>
