0007f60914404ec8e3ba8bd92a6f63e19bb7bfa9
AKJ <kjajila@gmail.com>
2022-11-10 13:39:31 -0500

    commit before pull

--- test/user.test.js
+++ test/user.test.js
@@ -1,101 +1,34 @@
-const TodoList = require("../todoList");
-const Todo = require("../todo");
+const User = require("../models/User");
 
-describe("TodoList", () => {
+describe("User", () => {
+  // Test for all use cases when initializing a new User object
   describe("Initialization", () => {
-    // Positive test
-    it("should create an object with a 'todos' property set to an empty array when called with the 'new' keyword", () => {
-      // Arrange
-      const todoList = new TodoList();
+    it("should create an object with a name and password if provided valid arguments", () => {
+      const user = new User("ballislyfe", "password");
 
-      // Assert
-      expect(todoList.todos).toEqual([]);
-    });
-  });
-
-  describe("addTodo", () => {
-    // Positive Tests
-    it("should add a new 'Todo' object to its 'todos' array", () => {
-      // Arrange
-      const todoList = new TodoList();
-      const todoText = "Mow Lawn";
-
-      // Act
-      todoList.addTodo(todoText);
-
-      // Assert
-      expect(todoList.todos.length).toEqual(1);
-      expect(todoList.todos[0] instanceof Todo).toEqual(true);
-      expect(todoList.todos[0].text).toEqual(todoText);
+      // Verify that the new object has the correct properties
+      expect(user.name).toEqual("ballislyfe");
+      expect(user.password).toEqual("password");
     });
 
-    // Exception test
-    it("should throw an error if not provided text", () => {
-      // Arrange
-      const todoList = new TodoList();
-      const err = new Error(
-        "Expected parameter 'text' to be a non empty string"
-      );
-      const cb = () => todoList.addTodo();
+    it("User name and password are mandatory. So should throw an error if provided no arguments", () => {
+  
+      const cb = () => new User();
+      expect(cb).toThrow();
+    });
 
-      // Assert
+    it("should throw an error if not provided a password", () => {
+      const cb = () => new User("ballislyfe");
+      const err = new Error("Expected password");
       expect(cb).toThrowError(err);
     });
-  });
 
-  describe("getNextTodo", () => {
-    // Positive test
-    it("should return the 0th todo element in the 'todos' array without removing it", () => {
-      // Arrange
-      const todoList = new TodoList();
-      const text1 = "Exercise";
-      const text2 = "Wash Car";
-      let nextTodo;
+    it("should throw an error if 'name' is not a string", () => {
+      const cb = () => new User(10, "test");
+      const err = new Error("Expected parameter 'name' to be a non-empty string");
 
-      // Act
-      todoList.addTodo(text1);
-      todoList.addTodo(text2);
-      nextTodo = todoList.getNextTodo();
-
-      // Assert
-      expect(nextTodo instanceof Todo).toEqual(true);
-      expect(nextTodo.text).toEqual(text1);
-      expect(todoList.todos.length).toEqual(2);
+      expect(cb).toThrowError(err);
     });
 
-    // Negative test
-    it("should return undefined if there are no todos", () => {
-      // Arrange
-      const todoList = new TodoList();
-      let nextTodo;
-
-      // Act
-      nextTodo = todoList.getNextTodo();
-
-      // Assert
-      expect(typeof nextTodo).toEqual("undefined");
-    });
-  });
-
-  describe("completeNextTodo", () => {
-    // Positive test
-    it("should return and remove the next todo in the list", () => {
-      // Arrange
-      const todoList = new TodoList();
-      const text1 = "Make Dinner";
-      const text2 = "Wash Dishes";
-      let nextTodo;
-
-      // Act
-      todoList.addTodo(text1);
-      todoList.addTodo(text2);
-      nextTodo = todoList.completeNextTodo();
-
-      // Assert
-      expect(nextTodo instanceof Todo).toEqual(true);
-      expect(nextTodo.text).toEqual(text1);
-      expect(todoList.todos.length).toEqual(1);
-      expect(todoList.getNextTodo().text).toEqual(text2);
-    });
   });
 });
