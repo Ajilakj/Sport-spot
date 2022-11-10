902cfe605fb4093eddf9b5e3b5c64e3af521cc30
Alice Shao <a1ice_shao@yahoo.com>
2022-11-10 12:17:51 -0500

    added passport to server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const passport = require("./config/passport");
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -29,23 +30,26 @@ const sess = {
     }),
   };
   
-  app.use(session(sess));
-  
-  const hbs = exphbs.create({ helpers });
-  
-  app.engine('handlebars', hbs.engine);
-  app.set('view engine', 'handlebars');
-  
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, 'public')));
-  
-  app.use(routes);
-  
-  sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () =>
-      console.log(
-        `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
-      )
-    );
-  });
+app.use(session(sess));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+    )
+  );
+});
