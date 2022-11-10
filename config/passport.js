c0931a502a7a90b55a4074fbaa2833b9271cf0c7
Alice Shao <a1ice_shao@yahoo.com>
2022-11-10 12:07:15 -0500

    added passport usage to verify user login

--- config/passport.js
+++ config/passport.js
@@ -3,9 +3,23 @@ const LocalStrategy = require("passport-local").Strategy;
 
 const db = require("../models");
 
-passport.use(new LocalStrategy((email, password, done) => {
+passport.use(new LocalStrategy(async(email, password, done) => {
     db.User.findOne({
         where: {email: email}
+    });
+    await((dbUser)=> {
+        if (!dbUser) {
+            return done(null, false, {
+                message: "No user found."
+            });
+        } else if (!dbUser.validPassword(password)){
+            return done(null, false, {
+                message: "Incorrect password."
+            });
+        }
+        return done(null, dbUser)
     })
 })
 )
+
+module.exports = passport;
