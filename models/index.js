32de228518b655815289c84f7661109eab3e06f2
AKJ <kjajila@gmail.com>
2022-11-10 09:32:44 -0500

    add foreign key

--- models/index.js
+++ models/index.js
@@ -3,27 +3,29 @@ const Sport = require("./Sport");
 const Post = require("./Post");
 
 User.hasMany(Sport, {
-    foreignKey: "sports_played"
+    foreignKey: "user_id"
 });
 
 Sport.belongsToMany(User, {
-    foreignKey: "sports_played"
+    //foreignKey: "sports_played"
+    through:{model:User}
 });
 
 User.hasMany(Post, {
     foreignKey: "user_id"
 });
 
-Post.belongsToOne(User, {
+Post.belongsTo(User, {
     foreignKey: "user_id"
 });
 
 Post.hasOne(Sport, {
-    foreignKey: "sport_name"
+    foreignKey: "sport_id"
 });
 
 Sport.belongsToMany(Post, {
-    foreignKey: "sport_name"
+//     foreignKey: "sport_name"
+    through:{model:Post}
 });
 
 module.exports = { User, Sport, Post };
