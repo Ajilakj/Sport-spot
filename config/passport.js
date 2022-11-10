8f9124c8949027c1c809b45f468491e8e2986f56
Alice Shao <a1ice_shao@yahoo.com>
2022-11-10 11:48:17 -0500

    added passport.js to config

---
+++ config/passport.js
@@ -0,0 +1,11 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const db = require("../models");
+
+passport.use(new LocalStrategy((email, password, done) => {
+    db.User.findOne({
+        where: {email: email}
+    })
+})
+)
