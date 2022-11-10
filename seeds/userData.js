29cb42a072d133c353a37c996183de84f9f705a4
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-11-10 13:47:03 -0500

    added some user seed data

--- seeds/userData.js
+++ seeds/userData.js
@@ -15,6 +15,34 @@ const userdata = [
     bio: 'Retired tennis pro looking for athletes to coach in NYC!',
     phone_number: '555-555-5556',
   },
+  {
+    username: 'soccerpro',
+    password: 'password',
+    email: 'soccerpro@email.com',
+    bio: 'Played some D1 soccer and just won the volo league championship!',
+    phone_number: '555-555-5556',
+  },
+  {
+    username: 'SerenaWilliams',
+    password: 'password',
+    email: 'serenawilliams@email.com',
+    bio: 'Retired tennis pro looking for athletes on my level to play pickup games in NYC!',
+    phone_number: '555-555-5556',
+  },
+  {
+    username: 'LionelMessi',
+    password: 'password',
+    email: 'lionelmessi@email.com',
+    bio: 'Looking for students to coach in soccer!',
+    phone_number: '555-555-5556',
+  },
+  {
+    username: 'VballMaster',
+    password: 'password',
+    email: 'vballmaster@email.com',
+    bio: 'Looking for fellow volleyball players to rent a sand court on Pier 25!',
+    phone_number: '555-555-5556',
+  },
 
 ];
 
