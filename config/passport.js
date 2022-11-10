33560181e2905ec5d11cd18f66d4cb7d00d33c22
Alice Shao <a1ice_shao@yahoo.com>
2022-11-10 12:23:40 -0500

    added comments for passport.js

--- config/passport.js
+++ config/passport.js
@@ -4,19 +4,23 @@ const LocalStrategy = require("passport-local").Strategy;
 const db = require("../models");
 
 passport.use(new LocalStrategy(async(email, password, done) => {
+    // looks for user
     db.User.findOne({
         where: {email: email}
     });
+    // checks for user
     await((dbUser)=> {
         if (!dbUser) {
             return done(null, false, {
                 message: "No user found."
             });
+        // checks for password
         } else if (!dbUser.validPassword(password)){
             return done(null, false, {
                 message: "Incorrect password."
             });
         }
+        // success
         return done(null, dbUser)
     })
 })
