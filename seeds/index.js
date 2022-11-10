30db5735e954c91b720c4cf0f8e87bef2f37b173
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-11-10 10:31:52 -0500

    updated index.js to include seedComment

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const sequelize = require('../config/connection');
 const seedSport = require('./sportData');
 const seedPost = require('./postData');
 const seedUser = require('./userData');
+const seedComment = require('./commentData');
 
 
 const seedAll = async () => {
@@ -10,6 +11,7 @@ const seedAll = async () => {
   await seedSport();
   await seedPost();
   await seedUser();
+  await seedComment();
 
   process.exit(0);
 };
