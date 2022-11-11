3e08148b0ebfab3782dde039ad52478de9103c3a
Valeria <alarconmvaleria@gmail.com>
2022-11-11 14:58:17 -0500

    Altering routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -99,5 +99,46 @@ router.get('/login', (req, res) => {
   });
 
 
+  // Login
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password. Please try again!' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password. Please try again!' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      console.log(
+        '🚀 ~ file: user-routes.js ~ req.session.save ~ req.session.cookie',
+        req.session.cookie
+      );
+
+      res
+        .status(200)
+        .json({ user: userData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
