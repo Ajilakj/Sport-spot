f0d36a8b7dc6f9a19d2b4d323e1e3b47ae758a4c
Alice Shao <a1ice_shao@yahoo.com>
2022-11-11 23:37:48 -0500

    added function to listen to comment submit

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,8 +1,18 @@
 const commentUser = document.getElementById("comment-user");
 const commentTime = document.getElementById("comment-time");
+const commentFormContent = document.getElementById("comment-from-content");
 const commentContent = document.getElementById("comment-content");
 const commentButton = document.getElementById("add-comment-btn");
 
-if (commentContent) {
 
-}
+commentButton.addEventListener("click", async()=> {
+    if (commentFormContent) {
+        const response = await fetch('/api/sport/post', {
+            method: 'POST',
+            body: JSON.stringify({ content }),
+            headers: { 'Content-Type': 'application/json' },
+          });
+        
+        commentContent.innerText = content;
+    }
+}) 
