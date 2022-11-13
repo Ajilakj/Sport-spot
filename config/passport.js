b54e6a5c470a58ab9a37eca2792d3767966b8aac
Alice Shao <a1ice_shao@yahoo.com>
2022-11-13 00:12:39 -0500

    fixed up config passport.js

--- config/passport.js
+++ config/passport.js
@@ -5,25 +5,27 @@ const db = require("../models");
 
 passport.use(new LocalStrategy(async(email, password, done) => {
     // looks for user
-    db.User.findOne({
-        where: {email: email}
-    });
-    // checks for user
-    await((dbUser)=> {
-        if (!dbUser) {
-            return done(null, false, {
-                message: "No user found."
-            });
-        // checks for password
-        } else if (!dbUser.validPassword(password)){
-            return done(null, false, {
-                message: "Incorrect password."
-            });
-        }
-        // success
-        return done(null, dbUser)
+    User.findOne(
+        { email: email }, function(err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false, {
+                    message: "No user found."
+                });
+            // checks for password
+            } 
+            if (!user.validPassword(password)){
+                return done(null, false, {
+                    message: "Incorrect password."
+                });
+            }
+            // success
+            return done(null, user)
+        })
     })
-})
-)
+    )
+
 
 module.exports = passport;
