ced51a2915626f1cbe69d61af7f80be06ffa11b5
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-11-14 15:23:46 -0500

    have posts poulating in descending order

--- controllers/api/blogpost-routes.js
+++ controllers/api/blogpost-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {User, Sport, Post } = require('../../models');
+const {Post } = require('../../models');
 const authMiddleware = require('../../utils/authMiddleware')
 
 
