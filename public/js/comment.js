ea5ed5da044e88b67b104adbc5652fa991abd71b
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-11-14 11:37:36 -0500

    working on comments

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,8 +1,5 @@
-const commentUser = document.getElementById("comment-user");
-const commentTime = document.getElementById("comment-time");
-const commentContent = document.getElementById("comment-content");
-const commentButton = document.getElementById("add-comment-btn");
+// const commentUser = document.getElementById("comment-user");
+// const commentTime = document.getElementById("comment-time");
+// const commentContent = document.getElementById("comment-content");
+// const commentButton = document.getElementById("add-comment-btn");
 
-if (commentContent) {
-
-}
