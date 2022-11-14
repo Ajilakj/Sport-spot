b576d0860510b4d4d67b766779a1396f56ad4ac0
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-11-13 20:41:41 -0500

    revised get route to include username and sport name to display on post details

--- controllers/api/post-details.js
+++ controllers/api/post-details.js
@@ -4,13 +4,33 @@ const { Sport, Post, User, Comment } = require('../../models');
 
 router.get('/:id', async (req, res) => {
     try {
-      const dbPostData = await Post.findByPk(req.params.id);
+      const dbPostData = await Post.findByPk(req.params.id, {
+        include: [
+            {
+                model: User,
+                attributes: [
+                   'username', 
+                ],
+                model: Sport,
+                attributes: [
+                    'name',
+                ]
+            }
+        ]
+      });
       const dbCommentData = await Comment.findAll(
         {
         where:
             {post_id: req.params.id}
-        });
-
+        },
+        // { 
+        // include: {
+        //     model: User,
+        //     attributes: [
+        //         'username',
+        //     ]
+        // }
+      );
       const post = dbPostData.get({ plain: true});
 
       const comment = dbCommentData.map((commentDetails) =>
